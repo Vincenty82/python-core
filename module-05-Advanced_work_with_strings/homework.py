27cfe58c8564debdec3e2c1d710aa59c7fdccedd
Vincenty82 <143327758+Vincenty82@users.noreply.github.com>
2023-12-05 23:28:42 +0100

    Update homework 5
    1 - Changed the function to use string method count() instead of a simple if counter.

--- module-05-Advanced_work_with_strings/homework.py
+++ module-05-Advanced_work_with_strings/homework.py
@@ -1,16 +1,59 @@
+# PUSTA FUNKCJA STARTOWA
+
+def empty_function():
+    return
+
 # ZADANIE 1
 
-def real_len(text):
-    count = 0
-    special_signs = ["\n", "\f", "\r", "\t", "\v"]
-    for chr in text:
-        if chr not in special_signs:
-            count += 1
-            print(chr, end=", ")
-    print(f"count = {count}")
-    return count
+def real_len(input_string):
+    non_escape_char_count = len(input_string)
+    escape_sequences = ["\n", "\f", "\r", "\t", "\v"]
+    for escape_sequence in escape_sequences:
+        non_escape_char_count -= input_string.count(escape_sequence)
+    return non_escape_char_count
+
+# ZADANIE 2
+
+articles_dict = [
+    {
+        "title": "Endless ocean waters.",
+        "author": "Jhon Stark",
+        "year": 2019,
+    },
+    {
+        "title": "Oceans of other planets are full of silver",
+        "author": "Artur Clark",
+        "year": 2020,
+    },
+    {
+        "title": "An ocean that cannot be crossed.",
+        "author": "Silver Name",
+        "year": 2021,
+    },
+    {
+        "title": "The ocean that you love.",
+        "author": "Golden Gun",
+        "year": 2021,
+    },
+]
+
+
+def find_articles(key, letter_case=False):
+    return
 
 # WYWOLANIA
 
 if __name__ == "__main__":
-    real_len("Alex\nKdfe23\t\f\v.\r")
+
+    # LISTA FUNKCJI DO WYKONANIA
+    
+    functions_to_execute = [
+        empty_function(),                                       # PUSTA FUNKCJA  
+        # real_len("Alex\nKdfe23\t\f\v.\r"),                      # ZADANIE 1
+        # find_articles(),                                        # ZADANIE 2
+    ]
+    
+    # WYKONANIE FUNKCJI I WYŚWIETLENIE WYNIKÓW
+    
+    for single_function in functions_to_execute:
+        print(f"return = {single_function}")
