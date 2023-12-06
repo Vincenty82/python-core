264c9fb2b7e3680ef1d0377a66623df565b6325c
Vincenty82 <143327758+Vincenty82@users.noreply.github.com>
2023-12-06 23:30:18 +0100

    Update homework 5
    Tasks 2, 3, 4 and 5.

--- module-05-Advanced_work_with_strings/homework.py
+++ module-05-Advanced_work_with_strings/homework.py
@@ -37,9 +37,55 @@ articles_dict = [
     },
 ]
 
-
 def find_articles(key, letter_case=False):
-    return
+    matching_articles = []
+    for article in articles_dict:
+        if letter_case:
+            if (key in article.get("title")) or (key in article.get("author")):
+                matching_articles.append(article)
+        else:
+            if (key.casefold() in article.get("title").casefold()) or (key.casefold() in article.get("author").casefold()):
+                matching_articles.append(article)
+    return matching_articles
+
+# ZADANIE 3
+
+def sanitize_phone_number(phone):
+    clean_phone = ""
+    for i in phone:
+        if i.isdigit():
+            clean_phone += i
+    return clean_phone
+
+# ZADANIE 4
+
+def is_check_name(fullname, first_name):
+    return fullname.startswith(first_name)
+
+# ZADANIE 5
+
+def get_phone_numbers_for_countries(list_phones):
+    Japan = []
+    Singapore = []
+    Taiwan = []
+    Ukraine = []
+    categorized_phones = {
+        "UA": Ukraine,
+        "JP": Japan,
+        "TW": Taiwan,
+        "SG": Singapore
+    }
+    for phone in list_phones:
+        new_phone = sanitize_phone_number(phone)
+        if new_phone.startswith("81"):
+            Japan.append(new_phone)
+        elif new_phone.startswith("65"):
+            Singapore.append(new_phone)
+        elif new_phone.startswith("886"):
+            Taiwan.append(new_phone)
+        else:
+            Ukraine.append(new_phone)
+    return categorized_phones
 
 # WYWOLANIA
 
@@ -48,12 +94,18 @@ if __name__ == "__main__":
     # LISTA FUNKCJI DO WYKONANIA
     
     functions_to_execute = [
-        empty_function(),                                       # PUSTA FUNKCJA  
-        # real_len("Alex\nKdfe23\t\f\v.\r"),                      # ZADANIE 1
-        # find_articles(),                                        # ZADANIE 2
+        # empty_function(),                                                                             # PUSTA FUNKCJA  
+        # real_len("Alex\nKdfe23\t\f\v.\r"),                                                            # ZADANIE 1
+        # find_articles("Ocean", True),                                                                 # ZADANIE 2
+        # sanitize_phone_number("    +38(050)123-32-34"),                                               # ZADANIE 3  
+        # is_check_name("Kevin Costner", "Kevin"),                                                      # ZADANIE 4            
+        # get_phone_numbers_for_countries(['380998759405', '818765347', '8867658976', '657658976'])     # ZADANIE 5                  
     ]
     
     # WYKONANIE FUNKCJI I WYŚWIETLENIE WYNIKÓW
     
-    for single_function in functions_to_execute:
-        print(f"return = {single_function}")
+    if functions_to_execute:
+        for single_function in functions_to_execute:
+            print(f"return = {single_function}")
+    else:
+        print("Wybierz funkcję z listy funkcji do wykonania.")
